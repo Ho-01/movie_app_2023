b01af8f7b3cc8ba032f240e9fa96b4059a7d036d
Ho-01 <kimhobig@gmail.com>
2023-06-21 21:17:19 +0900

    #3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+// App.js
+import React from 'react';
+
+function Food({fav}){
+  return <h3>I love {fav}</h3>
+}
 
 function App() {
-  return <div>Hello!!</div>
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi"/>
+      <Food fav="ramen"/>
+      <Food fav="samgiopsal"/>
+      <Food fav="chukumi"/>
+    </div>
+  );
 }
 
 export default App;
